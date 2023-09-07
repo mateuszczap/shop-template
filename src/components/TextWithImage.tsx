6d1759f2dcdead2740a803dc3add1b70a3aa2a0e
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-09-07 13:19:13 +0200

    add some style to TextWithImage

--- src/components/TextWithImage.tsx
+++ src/components/TextWithImage.tsx
@@ -1,16 +1,15 @@
 function TextWithImage () {
     return (
         <div className="bottom-container-img">
-            <div className="about-me">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!     
-            </div>
-            <div className="wood-img"></div>
-            <div className="about-me">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!</div>
-            <div className="about-me">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
+            <div className="about-me"><h4>Tytuł jakiś tam</h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!</div>
             <div className="wood-img"></div>
-            <div className="about-me">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
+            <div className="about-me"><h4>Tytuł jakiś tam</h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!</div>
+            <div className="about-me"><h4>Tytuł jakiś tam</h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!</div>
+            <div className="wood-img"></div>
+            <div className="about-me"><h4>Tytuł jakiś tam</h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga itaque doloribus eos impedit inventore labore, nemo sed excepturi eaque debitis soluta quaerat laudantium libero dignissimos. Rem cumque consectetur aperiam. Autem!</div>
         </div>
     )
