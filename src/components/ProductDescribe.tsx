127ab75586440b3a121f8d1d5a5087cc40688a36
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-09-28 19:46:01 +0200

    refresh code without failures

--- src/components/ProductDescribe.tsx
+++ src/components/ProductDescribe.tsx
@@ -1,7 +1,13 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
 
 function ProductDescribe () {
     return (
+        <>
+        <Header/>
         <div>COŚ</div>
+        <Footer/>
+        </>
     )
 }
 
