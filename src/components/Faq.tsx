0e6b07a9c3966fbc53ad3b46e33b5b0de6972935
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-10-09 09:50:38 +0200

    created array with faq data

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -2,6 +2,7 @@ import { FaqElement } from "./FaqElement";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import React, { useState } from 'react';
+import { faqData } from "../data/faq";
 
 function Faq() {
     const [isOpen, setIsOpen] = useState(false)
@@ -10,11 +11,11 @@ function Faq() {
         <Header/>
         <div>
             <div className="faq-questions"><h3>Najczęściej zadawane pytania (FAQ)</h3>
-                <FaqElement question={"Zadajmy jakieś pytanie?1"} answer={"To jest moja odpowiedź"}/>
-                <FaqElement question={"Zadajmy jakieś pytanie?2"} answer={"To jest moja odpowiedź"}/>
-                <FaqElement question={"Zadajmy jakieś pytanie?3"} answer={"To jest moja odpowiedź"}/>
-                <FaqElement question={"Zadajmy jakieś pytanie?4"} answer={"To jest moja odpowiedź"}/>
-                <FaqElement question={"Zadajmy jakieś pytanie?5"} answer={"To jest moja odpowiedź"}/>
+            {faqData.map((faq) => {
+                return <FaqElement question={faq.zapytanie} answer={faq.odpowiedź}/>
+            })}
+                
+              
                 </div>
         </div>
         <Footer/>
---
+++ src/data/faq.ts
@@ -0,0 +1,14 @@
+export const faqData = [
+    {
+        zapytanie: 'Zadaj mi pytanie 353768',
+        odpowiedź: 'Odpowiadam Ci fujaro',   
+    },
+    {
+        zapytanie: 'Zadaj mi pytanie ',
+        odpowiedź: 'Odpowiadam Ci fujaro',   
+    },
+    {
+        zapytanie: 'Zadaj mi pytanie sfgndtty',
+        odpowiedź: 'Odpowiadam Ci fujaro',   
+    },
+]
