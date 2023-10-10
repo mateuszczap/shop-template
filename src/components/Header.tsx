0b7fd7994adcbe1847d9134af838f577432ef882
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-10-10 18:08:38 +0200

    created navlink to main page from pecker logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@
 import { NavLink } from 'react-router-dom';
 import { headerData } from '../data/headerData';
 
+
 function Header() {
     return (
         <header className="header">
           <div>
-            <img src='/pecker-logo.png' width={100} height={100} className="imgHeader"/>
+            <NavLink to='/'>
+              <img src='/pecker-logo.png' width={100} height={100} className="imgHeader"/>
+            </NavLink>
           </div>
           <div className="navigationHeader">
             {headerData.map((item) => {
