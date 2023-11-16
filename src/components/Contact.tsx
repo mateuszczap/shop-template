c77b2b1b940c0dfdc966da60ca91f17668f5d813
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-11-16 09:08:18 +0100

    add

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,29 @@
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { SocialInfo } from "./SocialInfo";
 
 function Contact() {
     return(
         <>
         <Header/>
-        <div>KONTAKT</div>
+        <div>
+            <div className="contact-container">
+                <h2>
+                    <p>Daj znać, czy coś Ciebie zainrteresowało.</p>
+                    <p>Chętnie odpowiem na Twoje pytania.</p>
+                </h2>
+                <div className="email-input">
+                    <p>E-mail</p>
+                    <input placeholder="Wpisz swoj adres email."></input>
+                </div>
+                <div className="email-text">
+                    <p>Treść maila</p>
+                    <input placeholder="Wpisz o co chcesz apytać."></input>
+                </div>
+                <button>WYŚLIJ</button>
+            </div>
+        </div>
+        <SocialInfo></SocialInfo>
         <Footer/>
         </>
     )
