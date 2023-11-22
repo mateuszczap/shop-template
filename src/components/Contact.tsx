14bf7d7f3c8ac7c7582d4904b8b23f82704706b2
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-11-22 09:06:17 +0100

    styling mail contact container

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -23,25 +23,29 @@ function Contact() {
                     <p>Daj znać, czy coś Ciebie zainrteresowało.</p>
                     <p>Chętnie odpowiem na Twoje pytania.</p>
                 </h2>
-                <div className="email-input">
-                    <p>E-mail</p>
-                    <input placeholder="Wpisz swoj adres email." type='email' value={inputEmail} onChange={handleInputEmail}></input>
+                <div className="email-input-container">
+                    <div className="email-input">
+                        <p>E-mail</p>
+                        <input placeholder="Wpisz swoj adres email." type='email' value={inputEmail} onChange={handleInputEmail}></input>
+                    </div>
                 </div>
-                <div className="email-text">
-                    <p>Treść maila</p>
-                    <input placeholder="Wpisz o co chcesz apytać." value={inputValue} onChange={handleInputChange}></input>
+                <div className="email-text-container">
+                    <div className="email-text">
+                        <p>Treść maila</p>
+                        <input placeholder="Wpisz o co chcesz apytać." value={inputValue} onChange={handleInputChange}></input>
+                    </div>
+                    <button className="button-email" onClick={async()=>{
+                        const response = await fetch("http://localhost:3001/hello", {
+                            body:JSON.stringify({email: inputEmail, message: inputValue }),
+                            method:'post',
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                        });
+                        const movies = await response;
+                        console.log(movies);
+                    }}>WYŚLIJ</button>
                 </div>
-                <button onClick={async()=>{
-                    const response = await fetch("http://localhost:3001/hello", {
-                        body:JSON.stringify({email: inputEmail, message: inputValue }),
-                        method:'post',
-                        headers: {
-                            "Content-Type": "application/json"
-                          },
-                    });
-                    const movies = await response;
-                    console.log(movies);
-                }}>WYŚLIJ</button>
             </div>
         </div>
         <SocialInfo></SocialInfo>
